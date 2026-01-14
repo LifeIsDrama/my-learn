308b1938884e704ffb2fb950d538092c29b4564f
red2222 <red2222@DESKTOP-RI4BATK>
2026-01-14 16:00:06 +0800

    冒泡排序

---
+++ c-learn/c-learn/point_qsort.c
@@ -0,0 +1,55 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+#include <stdlib.h>
+
+
+void swap_item(char* el1, char* el2, int width) {
+	
+	char tmp;
+	for (int i = 0; i < width; i++)
+	{
+		tmp = *(el1 + i);
+		*(el1 + i) = *(el2 + i);
+		*(el2 + i) = tmp;
+	}
+	int a;
+}
+
+void my_qsort(void* arr_p, size_t size, size_t width, int(*cmp)(void* el1, void* el2)) {
+	
+
+	// 1 2 3 4 5  size = 5
+	// 3  5 - 1 -3
+
+	for (size_t i = 0; i < size - 1; i++)
+	{
+		for (size_t j = 0; j < size - 1 - i; j++)
+		{
+			if(cmp((char*)arr_p+ width *j, (char*)arr_p+width*(j+1)) > 0) {
+				swap_item((char*)arr_p + width * j, (char*)arr_p + width * (j + 1), width);
+			}
+		}
+	}
+
+}
+
+int cmp(void* el1, void* el2) {
+	return *(int*) el1 - *(int*) el2;
+}
+
+
+
+
+int main() {
+	int arr[] = { 4,5,7,8,9,3 };
+	int size = sizeof(arr)/ sizeof(arr[0]);
+	my_qsort(arr, size, sizeof(arr[0]), cmp);
+
+
+	for (int i = 0; i < size; i++)
+	{
+		printf("%d ", arr[i]);
+	}
+
+	
+}
