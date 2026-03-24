a442651111fdffdc52ef00c75f4fcd5b8414c2dc
red2222 <466483135@qq.com>
2026-03-24 19:25:41 +0800

    set email

---
+++ C#-learns/C#-learn/chapter4/表达式.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C__learn.chapter4
+{
+	internal class 表达式
+	{
+		public static void calc() {
+			int i = 927;
+			int j = 112;
+			Console.WriteLine(Math.Sin(i * i + j * j));
+		}
+	}
+}
